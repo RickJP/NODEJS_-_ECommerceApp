8b3b53b2a8a27183f6e6b4c793045e5071522600
Rick <{ID}+{username}@users.noreply.github.com>
2019-10-13 23:33:48 +0900

    ADD: Completed Product Validation

--- controllers/product.js
+++ controllers/product.js
@@ -13,9 +13,29 @@ exports.create = (req, res) => {
         error: 'Image could not be uploaded'
       });
     }
+    // check for all fields
+    const { name, description, price, category, quantity, shipping  } = fields;
+
+    
+    if (!name || !description || !price || !category || !quantity || !shipping) {
+      console.log('NAME:    ' + name);
+      return res.status(400).json({
+        error: "All fields are required"
+      });
+    }
+
     let product = new Product(fields);
 
+    // 1kb = 1000
+    // 1mb = 1000_000
+
     if (files.photo) {
+      console.log(`FILES_PHOTO: ${files.photo}`);
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image should be less than 1mb in size"
+        });
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
