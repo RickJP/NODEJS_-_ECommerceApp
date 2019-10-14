e5da8780b58c52a6afe487ad6595b96dcec54664
Rick <{ID}+{username}@users.noreply.github.com>
2019-10-14 22:06:10 +0900

    ADD: Query Related Products

--- routes/product.js
+++ routes/product.js
@@ -3,12 +3,17 @@ const router = express.Router();
 
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 const { userById } = require('../controllers/user');
-const { create, productById, read, remove, update } = require('../controllers/product');
+const { create, productById, read, remove, update, list , listRelated} = require('../controllers/product');
 
 router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
+
+router.get('/products', list);
+router.get('/products/related/:productId', listRelated);
+
+
 
 router.param("userId", userById);
 router.param("productId", productById);
