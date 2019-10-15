228f1af63cf363d5d06569861bce6861bfe829a0
Rick <{ID}+{username}@users.noreply.github.com>
2019-10-15 18:01:26 +0900

    ADD: List Products By Search

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 const { userById } = require('../controllers/user');
-const { create, productById, read, remove, update, list , listRelated, listCategories} = require('../controllers/product');
+const { create, productById, read, remove, update, list , listRelated, listCategories, listBySearch} = require('../controllers/product');
 
 router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
@@ -15,6 +15,7 @@ router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update
 router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
 router.get('/products/categories', listCategories);
+router.post('/products/by/search', listBySearch);
 
 router.param("userId", userById);
 router.param("productId", productById);
