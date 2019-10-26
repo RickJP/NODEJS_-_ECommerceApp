5314e1137181a01d5e2a03f0850d29c4677ced7e
Rick <{ID}+{username}@users.noreply.github.com>
2019-10-26 15:41:13 +0900

    ADD: Find order by id; update order status

--- routes/order.js
+++ routes/order.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, listOrders} = require('../controllers/order');
+const { create, listOrders, getStatusValues, orderById, updateOrderStatus} = require('../controllers/order');
 const {decreaseQuantity} = require('../controllers/product');
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 const {userById, addOrderToUserHistory} = require('../controllers/user');
 
 
 router.get('/order/list/:userId', requireSignin, isAuth, isAdmin, listOrders);
+router.get('/order/status-values/:userId', requireSignin, isAuth, isAdmin, getStatusValues);
 
 router.post(
   '/order/create/:userId',
@@ -18,6 +19,16 @@ router.post(
   create
 );
 
+router.put(
+  '/order/:orderId/status/:userId',
+  requireSignin,
+  isAuth,
+  isAdmin,
+  updateOrderStatus
+);
+
+
 router.param('userId', userById);
+router.param('orderId', orderById);
 
 module.exports = router;
